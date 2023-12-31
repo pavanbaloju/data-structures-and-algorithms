483ab0e083e19d5342d87a917d0c47d6b0c43620
Pavan Baloju <pavanbaloju9@gmail.com>
2023-12-31 20:31:02 +0530

    [150] max depth of tree

---
+++ 150/53.MaxDepthOfTree.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+int maxDepth(TreeNode *root)
+{
+    if (!root)
+    {
+        return 0;
+    }
+    return 1 + max(maxDepth(root->left), maxDepth(root->right));
+}
+int main()
+{
+    TreeNode *root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(4);
+
+    cout << maxDepth(root);
+    return 0;
+}

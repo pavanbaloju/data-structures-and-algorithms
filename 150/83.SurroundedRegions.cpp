ccd643024779228e0310517d50a79307c42add2e
Pavan Baloju <pavanbaloju9@gmail.com>
2024-01-10 07:11:57 +0530

    [150] surrrounded regions in a grid

--- 150/83.SurroundedRegions.cpp
+++ 150/83.SurroundedRegions.cpp
@@ -64,6 +64,59 @@ void solve(vector<vector<char>> &board)
     }
 }
 
+// with out additional space for visited
+void helper2(vector<vector<char>> &board, int r, int c)
+{
+    if (r < 0 || r >= board.size() || c < 0 || c >= board[0].size() || board[r][c] == 'X' || board[r][c] == 'M')
+        return;
+
+    board[r][c] = 'M';
+    for (auto nrc : vector<pair<int, int>>({{-1, 0}, {1, 0}, {0, -1}, {0, 1}}))
+        helper2(board, r + nrc.first, c + nrc.second);
+}
+
+void solve2(vector<vector<char>> &board)
+{
+    int R = board.size(), C = board[0].size();
+
+    for (int r : vector<int>({0, R - 1}))
+    {
+        for (int c = 0; c < C; c++)
+        {
+            if (board[r][c] == 'O')
+            {
+                helper2(board, r, c);
+            }
+        }
+    }
+
+    for (int c : vector<int>({0, C - 1}))
+    {
+        for (int r = 1; r < R - 1; r++)
+        {
+            if (board[r][c] == 'O')
+            {
+                helper2(board, r, c);
+            }
+        }
+    }
+
+    for (int r = 0; r < R; r++)
+    {
+        for (int c = 0; c < C; c++)
+        {
+            if (board[r][c] == 'O')
+            {
+                board[r][c] = 'X';
+            }
+            else if (board[r][c] == 'M')
+            {
+                board[r][c] = 'O';
+            }
+        }
+    }
+}
+
 int main()
 {
     vector<vector<char>> grid =

66f18af4a23aefcde896f96a819e24b0edd5e89f
Pavan Baloju <pavanbaloju9@gmail.com>
2024-01-12 07:24:24 +0530

    [150] dp repeat problems

---
+++ 150/91.HouseRobber.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int rob(vector<int> &nums)
+{
+    int n = nums.size();
+    int prev = nums[0], prev2 = 0;
+
+    for (int i = 1; i < n; i++)
+    {
+        int pick = nums[i], curr = 0;
+        if (i > 1)
+            pick += prev2;
+        int nonPick = prev;
+
+        curr = max(pick, nonPick);
+        prev2 = prev;
+        prev = curr;
+    }
+    return prev;
+}
+
+int main()
+{
+    vector<int> nums{1, 2, 3, 1};
+    cout << rob(nums) << endl;
+    return 0;
+}
